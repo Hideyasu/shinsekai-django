2175707719ec148f09d163692fd2d79ec3dc7eda
Hideyasu <kutuzov1228@gmail.com>
2024-09-22 10:22:10 +0900

    feat: allow all hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_3kyc6!ghdl(dd(2%04#*2_8-vxz#zl4*7$o_$kg!nv1+&_&ib
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
